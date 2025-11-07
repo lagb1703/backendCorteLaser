833f4c362dd03a510bb5122886a4061f6258bac7
Luis Alejandro Giraldo Bolaños <luis.giraldo3@utp.edu.co>
2025-11-06 19:16:12 -0500

    reset

--- src/PaymentModule/enums/__init__.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-class ExceptionsEnum(Enum):
-    TIME_OUT_EXCEPTION="No se logro alcanzar el destino"
-    WOMPY_CONECTION_ERROR="Puta madre, se cayo bancolombia"
-    WOMPY_DNS_ERROR="Error en la dns"
-    WOMPY_NO_KNOWLEDGE_ERROR="ni idea de lo que paso"
